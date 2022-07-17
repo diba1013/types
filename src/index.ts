831fa5e0dc02ea9a6a978f7fe830beb7550a14aa
diba1013 <diba1013@web.de>
2022-07-17 16:02:29 +0200

    feat: add basic types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import path from "path";
+/**
+ * The current version of the package.
+ */
+export const VERSION = "__VERSION__";
 
-console.log(path.dirname(process.cwd()));
+/**
+ * For those libraries that will not accept undefined.
+ */
+export const NONE = null; // eslint-disable-line unicorn/no-null
+
+/**
+ * An value that is either present or absent.
+ */
+export type Optional<T> = T | undefined;
+
+/**
+ * An eventually resolved value.
+ */
+export type MaybePromise<T> = T | PromiseLike<T> | Promise<T>;
